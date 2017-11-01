bd377e88989990edbaa3af047ee9471d7094054d
Adli Fariz Bonaputra <abathz@gmail.com>
2017-11-01 20:12:16 +0700

    update 01-11-2017 refactor repos => success

--- src/reducers/reducers_transaction.js
+++
@@ -1,9 +0,0 @@
-import { GET_ALL_TRANSACTION } from 'actions/const'
-
-export default function (state = {}, action) {
-  switch (action.type) {
-    case GET_ALL_TRANSACTION:
-      return action.payload
-  }
-  return state
-}
